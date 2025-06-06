276a92c8858d430cdcbc74b90ff724680a04d421
Tolga Zorlu <tolgazorlu@Mac.home>
2025-06-06 22:31:31 +0300

    Add Hero component with Tailwind CSS styling

---
+++ src/components/hero.ts
@@ -0,0 +1,13 @@
+export function Hero(): string {
+    return `
+    <section class="bg-white dark:bg-gray-900 h-96 w-full flex items-center justify-center">
+    
+        <!-- Background Image -->
+        <img src="http://www.teamk6.games/wp-content/uploads/2025/05/lhero-scaled.png" alt="Hero" class="top-0 left-0 w-full h-full object-cover opacity-10">
+        <!-- End Background Image -->
+    
+       
+
+    </section>
+  `
+}
