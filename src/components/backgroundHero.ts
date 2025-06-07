9299c98693e0d8f59d4b596d99db89ae5359cbaf
Tolga Zorlu <tolgazorlu@Mac.home>
2025-06-07 19:31:13 +0300

    Update BackgroundHero component to use a fixed position and replace placeholder image with a local asset

--- src/components/backgroundHero.ts
+++ src/components/backgroundHero.ts
@@ -1,8 +1,8 @@
 export function BackgroundHero(): string {
-    return `
-    <section class="bg-white dark:bg-gray-900 h-96 w-full flex items-center justify-center">
+  return `
+    <section class="fixed bg-white dark:bg-gray-900 h-screen w-full flex items-center justify-center -z-10">
         <!-- Background Image -->
-        <img src="https://placehold.co/400" alt="Hero" class="top-0 left-0 w-full h-full object-cover opacity-10">
+        <img src="/assets/background.png" alt="Hero" class="top-0 left-0 w-full h-full object-cover opacity-10">
         <!-- End Background Image -->
     </section>
   `
